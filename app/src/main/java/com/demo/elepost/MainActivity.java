dc75ab541cdb12c3dac5b2bf12a88831037d29ed
GodYihan <hyh739146825@gmail.com>
2020-09-10 23:59:11 +0800

    1.0.1-beta

--- app/src/main/java/com/demo/elepost/MainActivity.java
+++ app/src/main/java/com/demo/elepost/MainActivity.java
@@ -3,15 +3,17 @@ package com.demo.elepost;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,9 +34,15 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mEt_point;
     private Button mBt_search;
+    private Button mBt_history;
     private Spinner mSn_area;
     private TextView mTv_result;
     private String returnResult;
+
+    private SharedPreferences sp;
+    private Editor spEditor;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +51,16 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("MainActivity", "Created!");
 
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
+        mContext = this;
+
+        sp = this.getSharedPreferences("cacheXML", MODE_PRIVATE);
+        spEditor = sp.edit();
+
+
+//        if (android.os.Build.VERSION.SDK_INT > 9) {
+//            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//            StrictMode.setThreadPolicy(policy);
+//        }
 
         mEt_point = findViewById(R.id.point);
         mBt_search = findViewById(R.id.btn_search);
@@ -56,7 +70,24 @@ public class MainActivity extends AppCompatActivity {
         final StringBuffer postInfo = new StringBuffer();
         final String url = "https://wx.ecnu.edu.cn/CorpWeChat/card/dogetEle.html";
 
+        area[0] = sp.getString("area", "TP");
+        point[0] = sp.getString("point", "");
+
+        mEt_point.setText(point[0]);
+
         mSn_area = findViewById(R.id.areaSpinner);
+
+        switch (area[0]) {
+            case "TP":
+                mSn_area.setSelection(0);
+                break;
+            case "zb":
+                mSn_area.setSelection(1);
+                break;
+            case "mh":
+                mSn_area.setSelection(2);
+                break;
+        }
 
         mSn_area.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -105,22 +136,51 @@ public class MainActivity extends AppCompatActivity {
                 if(point[0] == null || point[0].equals("")){
                     Toast.makeText(MainActivity.this,"请输入房间号！", LENGTH_SHORT).show();
                 }else{
-                    Connection resultHtml = Jsoup.connect(url);
+                    final Connection resultHtml = Jsoup.connect(url);
                     resultHtml.data("area", area[0]);
                     resultHtml.data("point",point[0]);
-                    Document resultDocument = null;
-                    try {
-                        resultDocument = resultHtml.post();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Element resultEle = resultDocument.selectFirst("h3");
-                    returnResult = resultEle.text();
-                    System.out.println("====result====>"+returnResult);
-                    postInfo.delete(0,postInfo.length());
-                    mTv_result = findViewById(R.id.mTv_result);
-                    mTv_result.setText(returnResult);
+
+                    spEditor.putString("area", area[0]);
+                    spEditor.putString("point", point[0]);
+                    spEditor.apply();
+
+                    final Document[] resultDocument = {null};
+
+                    Thread postThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                resultDocument[0] = resultHtml.post();
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if(resultDocument[0] != null){
+                                            Element resultEle = resultDocument[0].selectFirst("h3");
+                                            returnResult = resultEle.text();
+                                        }else {
+                                            Toast.makeText(mContext,"请检查网络", LENGTH_SHORT).show();
+                                        }
+                                        postInfo.delete(0,postInfo.length());
+                                        mTv_result = findViewById(R.id.mTv_result);
+                                        mTv_result.setText(returnResult);
+                                    }
+                                });
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    postThread.start();
                 }
+            }
+        });
+
+        mBt_history = findViewById(R.id.btn_history);
+        mBt_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
             }
         });
     }
